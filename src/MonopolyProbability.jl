f300a7eab9f520e3b273ab7db391498acffda532
RemiLequette <remi.lequette@laposte.net>
2026-02-18 16:17:23 +0100

    Add US standard board function and update exports

--- src/MonopolyProbability.jl
+++ src/MonopolyProbability.jl
@@ -1,10 +1,23 @@
 module MonopolyProbability
 
-export BOARD_SIZE, standard_board
+export BOARD_SIZE, standard_board, standard_board_us
 
 const BOARD_SIZE = 40
 
 function standard_board()
+    return [
+        "Départ", "Boulevard de Belleville", "Caisse de communauté", "Rue Lecourbe", "Impôt sur le revenu",
+        "Gare Montparnasse", "Rue de Vaugirard", "Chance", "Rue de Courcelles", "Avenue de la République",
+        "Prison / Simple visite", "Boulevard de la Villette", "Compagnie d'électricité", "Avenue de Neuilly", "Rue de Paradis",
+        "Gare de Lyon", "Avenue Mozart", "Caisse de communauté", "Boulevard Saint-Michel", "Place Pigalle",
+        "Parc Gratuit", "Avenue Matignon", "Chance", "Boulevard Malesherbes", "Avenue Henri-Martin",
+        "Gare du Nord", "Faubourg Saint-Honoré", "Place de la Bourse", "Compagnie des eaux", "Rue La Fayette",
+        "Allez en prison", "Avenue de Breteuil", "Avenue Foch", "Caisse de communauté", "Boulevard des Capucines",
+        "Gare Saint-Lazare", "Chance", "Avenue des Champs-Élysées", "Taxe de luxe", "Rue de la Paix"
+    ]
+end
+
+function standard_board_us()
     return [
         "GO", "Mediterranean Avenue", "Community Chest", "Baltic Avenue", "Income Tax",
         "Reading Railroad", "Oriental Avenue", "Chance", "Vermont Avenue", "Connecticut Avenue",
